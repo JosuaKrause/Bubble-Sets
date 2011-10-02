1db6683ed853535f287c2357f06383a92bb86d29
Joschi <josua.krause@googlemail.com>
2011-10-02 15:26:28 +0200

    bsplines finished

--- src/setvis/shape/BSplineShapeGenerator.java
+++ src/setvis/shape/BSplineShapeGenerator.java
@@ -1,10 +1,5 @@
 package setvis.shape;
 
-import java.awt.Font;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
-import java.awt.Point;
-import java.awt.Polygon;
 import java.awt.Shape;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Point2D;
@@ -19,137 +14,80 @@ import setvis.SetOutline;
  */
 public class BSplineShapeGenerator extends RoundShapeGenerator {
 
-	/**
-	 * Creates a new {@link BSplineShapeGenerator} with the given set outline
-	 * creator.
-	 * 
-	 * @param outline
-	 *            The set outline generator.
-	 */
-	public BSplineShapeGenerator(final SetOutline outline) {
-		super(outline, true);
-	}
+    // since the basic function is fixed this value should not be changed
+    private static final int ORDER = 3;
 
-	@Override
-	public Shape convertToShape(final Point2D[] points, final boolean closed) {
-		final GeneralPath res = new GeneralPath();
-		// final Point2D last = null;
-		// for (int i = 0; i < points.length; ++i) {
-		//
-		// }
+    private static final int START_INDEX = ORDER - 1;
 
-		// TODO Auto-generated method stub
-		return res;
-	}
+    private static final int REL_END = 1;
 
-	// the basis function for a cubic B spline
-	float b(final int i, final float t) {
-		switch (i) {
-		case -2:
-			return (((-t + 3) * t - 3) * t + 1) / 6;
-		case -1:
-			return (((3 * t - 6) * t) * t + 4) / 6;
-		case 0:
-			return (((-3 * t + 3) * t + 3) * t + 1) / 6;
-		case 1:
-			return (t * t * t) / 6;
-		}
-		return 0; // we only get here if an invalid i is specified
-	}
+    private static final int REL_START = REL_END - ORDER;
 
-	// evaluate a point on the B spline
-	Point p(final int i, final float t) {
-		float px = 0;
-		float py = 0;
-		for (int j = -2; j <= 1; j++) {
-			px += b(j, t) * pts.xpoints[i + j];
-			py += b(j, t) * pts.ypoints[i + j];
-		}
-		return new Point(Math.round(px), Math.round(py));
-	}
+    private int granularity = 12;
 
-	final int STEPS = 12;
+    /**
+     * Creates a new {@link BSplineShapeGenerator} with the given set outline
+     * creator.
+     * 
+     * @param outline
+     *            The set outline generator.
+     */
+    public BSplineShapeGenerator(final SetOutline outline) {
+        super(outline, true);
+    }
 
-	public void paint(final Graphics g) {
-		final FontMetrics fm = g.getFontMetrics(f);
-		g.setFont(f);
-		final int h = fm.getAscent() / 2;
+    public void setGranularity(final int granularity) {
+        this.granularity = granularity;
+    }
 
-		for (int i = 0; i < pts.npoints; i++) {
-			final String s = Integer.toString(i);
-			final int w = fm.stringWidth(s) / 2;
-			g.drawString(Integer.toString(i), pts.xpoints[i] - w,
-					pts.ypoints[i] + h);
-		}
-		final Polygon pol = new Polygon();
-		Point q = p(2, 0);
-		pol.addPoint(q.x, q.y);
-		for (int i = 2; i < pts.npoints - 1; i++) {
-			for (int j = 1; j <= STEPS; j++) {
-				q = p(i, j / (float) STEPS);
-				pol.addPoint(q.x, q.y);
-			}
-		}
-		g.drawPolyline(pol.xpoints, pol.ypoints, pol.npoints);
-	}
+    public int getGranularity() {
+        return granularity;
+    }
 
-	protected Polygon pts = new Polygon();
-	protected int selection = -1;
+    @Override
+    public Shape convertToShape(final Point2D[] points, final boolean closed) {
+        final GeneralPath res = new GeneralPath();
+        final int count = (closed ? points.length + ORDER : points.length) - 1;
+        final double g = granularity;
+        final Point2D start = calcPoint(points, START_INDEX, 0);
+        res.moveTo(start.getX(), start.getY());
+        for (int i = START_INDEX; i < count; ++i) {
+            for (int j = 1; j <= granularity; ++j) {
+                final Point2D cur = calcPoint(points, i, j / g);
+                res.lineTo(cur.getX(), cur.getY());
+            }
+        }
+        return res;
+    }
 
-	static Font f = new Font("Courier", Font.PLAIN, 12);
+    private static double basicFunction(final int i, final double t) {
+        // the basis function for a cubic B spline
+        switch (i) {
+        case -2:
+            return (((-t + 3) * t - 3) * t + 1) / 6;
+        case -1:
+            return (((3 * t - 6) * t) * t + 4) / 6;
+        case 0:
+            return (((-3 * t + 3) * t + 3) * t + 1) / 6;
+        case 1:
+            return (t * t * t) / 6;
+        default:
+            throw new InternalError();
+        }
+    }
 
-	static final int EPSILON = 36; /* square of distance for picking */
+    // evaluates a point on the B spline
+    private Point2D calcPoint(final Point2D[] points, final int i,
+            final double t) {
+        double px = 0;
+        double py = 0;
+        for (int j = REL_START; j <= REL_END; j++) {
+            final Point2D p = points[getRelativeIndex(i, j, points.length)];
+            final double bf = basicFunction(j, t);
+            px += bf * p.getX();
+            py += bf * p.getY();
+        }
+        return new Point2D.Double(px, py);
+    }
 
-	/* return index of control point near to (x,y) or -1 if nothing near */
-	public int selectPoint(final int x, final int y) {
-		int mind = Integer.MAX_VALUE;
-		selection = -1;
-		for (int i = 0; i < pts.npoints; i++) {
-			final int d = sqr(pts.xpoints[i] - x) + sqr(pts.ypoints[i] - y);
-			if (d < mind && d < EPSILON) {
-				mind = d;
-				selection = i;
-			}
-		}
-		return selection;
-	}
-
-	// square of an int
-	static int sqr(final int x) {
-		return x * x;
-	}
-
-	/* add a control point, return index of new control point */
-	public int addPoint(final int x, final int y) {
-		pts.addPoint(x, y);
-		return selection = pts.npoints - 1;
-	}
-
-	/* set selected control point */
-	public void setPoint(final int x, final int y) {
-		if (selection >= 0) {
-			pts.xpoints[selection] = x;
-			pts.ypoints[selection] = y;
-		}
-	}
-
-	/** remove selected control point */
-	public void removePoint() {
-		if (selection >= 0) {
-			pts.npoints--;
-			for (int i = selection; i < pts.npoints; i++) {
-				pts.xpoints[i] = pts.xpoints[i + 1];
-				pts.ypoints[i] = pts.ypoints[i + 1];
-			}
-		}
-	}
-
-	@Override
-	public String toString() {
-		final StringBuffer result = new StringBuffer();
-		for (int i = 0; i < pts.npoints; i++) {
-			result.append(" " + pts.xpoints[i] + " " + pts.ypoints[i]);
-		}
-		return result.toString();
-	}
 }
--- src/setvis/shape/RoundShapeGenerator.java
+++ src/setvis/shape/RoundShapeGenerator.java
@@ -17,37 +17,42 @@ import setvis.SetOutline;
  */
 public abstract class RoundShapeGenerator extends AbstractShapeCreator {
 
-	/**
-	 * Whether the result of the set outlines are interpreted in clockwise
-	 * order.
-	 */
-	private final boolean clockwise;
+    /**
+     * Whether the result of the set outlines are interpreted in clockwise
+     * order.
+     */
+    private final boolean clockwise;
 
-	/**
-	 * Creates an {@link RoundShapeGenerator} with a given set outline creator.
-	 * 
-	 * @param outline
-	 *            The creator of the set outlines.
-	 * @param clockwise
-	 *            Whether the result of the set outlines are interpreted in
-	 *            clockwise order.
-	 */
-	public RoundShapeGenerator(final SetOutline outline, final boolean clockwise) {
-		super(outline);
-		this.clockwise = clockwise;
-	}
+    /**
+     * Creates an {@link RoundShapeGenerator} with a given set outline creator.
+     * 
+     * @param outline
+     *            The creator of the set outlines.
+     * @param clockwise
+     *            Whether the result of the set outlines are interpreted in
+     *            clockwise order.
+     */
+    public RoundShapeGenerator(final SetOutline outline, final boolean clockwise) {
+        super(outline);
+        this.clockwise = clockwise;
+    }
 
-	/**
-	 * @param index
-	 *            The index.
-	 * @param len
-	 *            The maximum length of the array.
-	 * @param next
-	 *            Whether to return the next or previous index.
-	 * @return The next or previous index for an array with the given length.
-	 */
-	protected final int getOtherIndex(final int index, final int len,
-			final boolean next) {
-		return ((next ^ clockwise) ? (index + len - 1) : (index + 1)) % len;
-	}
+    /**
+     * @param index
+     *            The index.
+     * @param len
+     *            The maximum length of the array.
+     * @param next
+     *            Whether to return the next or previous index.
+     * @return The next or previous index for an array with the given length.
+     */
+    protected final int getOtherIndex(final int index, final int len,
+            final boolean next) {
+        return ((next ^ clockwise) ? (index + len - 1) : (index + 1)) % len;
+    }
+
+    protected final int getRelativeIndex(final int index, final int relIndex,
+            final int len) {
+        return (index + (clockwise ? relIndex : -relIndex)) % len;
+    }
 }
